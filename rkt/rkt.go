fd387a141cf46c20f1fed5fac43305b1c99e9a47
Jonathan Boulle <jonathanboulle@gmail.com>
2014-11-13 14:22:40 -0800

    rkt: spit out a basic container manifest

--- rkt/rkt.go
+++ rkt/rkt.go
@@ -22,20 +22,28 @@ package main
 //
 
 import (
+	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
+	"log"
 	"os"
 	"strings"
+
+	// WARNING: here be dragons
+	// TODO(jonboulle): vendor this once the schema is stable
+	"github.com/containers/standard/schema"
 )
 
 var (
 	fs = flag.NewFlagSet("rkt", flag.ExitOnError)
 
-	flagVolumes stringSlice
+	flagVolumes volumeMap
 )
 
 func init() {
 	fs.Var(&flagVolumes, "volume", "volumes to mount into the shared container environment")
+	flagVolumes = volumeMap{}
 }
 
 func main() {
@@ -53,21 +61,81 @@ func main() {
 		os.Exit(1)
 	}
 	fs.Parse(args[1:])
-	images := fs.Args()
-	fmt.Println("run rocket run")
-	fmt.Printf("images: %s\n", images)
-	fmt.Printf("volumes: %s\n", flagVolumes)
+	apps := fs.Args()
+	cm := &schema.ContainerManifest{
+		ACType: "ContainerManifest",
+	}
+
+	v, err := schema.NewSemVer("1.0.0")
+	if err != nil {
+		log.Fatalf("error creating version: %v", err)
+	}
+	cm.ACVersion = *v
+
+	cuid, err := schema.NewUUID(genUID())
+	if err != nil {
+		log.Fatalf("error creating UID: %v", err)
+	}
+	cm.UID = *cuid
+
+	sApps := make(map[string]schema.App)
+	for _, name := range apps {
+		a, err := newApp(name)
+		if err != nil {
+			log.Fatalf("error creating app: %v", err)
+		}
+		sApps[name] = *a
+	}
+	cm.Apps = sApps
+
+	sVols := make(map[string]schema.Volume)
+	for key, path := range flagVolumes {
+		sVols[key] = schema.Volume{path}
+	}
+	cm.Volumes = sVols
+
+	b, err := json.Marshal(cm)
+	if err != nil {
+		log.Fatalf("error marshalling container manifest: %v", err)
+	}
+	fmt.Println(string(b))
 }
 
-// stringSlice implements the flag.Value interface
-type stringSlice []string
+// newApp creates a new App
+// TODO(jonboulle): implement me
+func newApp(name string) (*schema.App, error) {
+	a := schema.App{
+		ID: name,
+	}
+	return &a, nil
+}
 
-func (ss *stringSlice) Set(s string) error {
-	// TODO(jonboulle): validate
-	*ss = append(*ss, s)
+// genUID generates a unique ID for the container
+// TODO(jonboulle): implement me
+func genUID() string {
+	return "6733C088-A507-4694-AABF-EDBE4FC5266F"
+}
+
+// volumeMap implements the flag.Value interface
+type volumeMap map[string]string
+
+func (vm *volumeMap) Set(s string) error {
+	elems := strings.Split(s, ":")
+	if len(elems) != 2 {
+		return errors.New("volume must be of form key:path")
+	}
+	key := elems[0]
+	if _, ok := (*vm)[key]; ok {
+		return fmt.Errorf("got multiple flags for volume %q", key)
+	}
+	(*vm)[key] = elems[1]
 	return nil
 }
 
-func (ss *stringSlice) String() string {
-	return strings.Join(*ss, ",")
+func (vm *volumeMap) String() string {
+	var ss []string
+	for k, v := range *vm {
+		ss = append(ss, fmt.Sprintf("%s:%s", k, v))
+	}
+	return strings.Join(ss, ",")
 }
