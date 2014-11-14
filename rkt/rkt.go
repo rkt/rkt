50cda71331fda3e599d03a719a4672b83a4f2485
Jonathan Boulle <jonathanboulle@gmail.com>
2014-11-13 22:09:17 -0800

    rkt: add TAF extraction and stage1

--- rkt/rkt.go
+++ rkt/rkt.go
@@ -6,11 +6,13 @@ package main
 // Execution on Rocket is divided into a number of stages, and the `rkt`
 // binary implements the first stage (stage 0), which consists of the
 // following tasks:
-// - Generating the Container Unique ID (UID)
-// - Generating the container document
-// - Creating a directory for the container
-// - Copying the stage1 into the container directory
-// - Copying the RAFs for each app into the stage2 directories
+// - Generating a Container Unique ID (UID)
+// - Generating a Container Runtime Manifest
+// - Creating a filesystem for the container
+// - Setting up stage 1 and stage 2 directories in the filesystem
+// - Copying the stage1 binary into the container filesystem
+// - Fetching the TAFs for the specified applications
+// - Unpacking the TAFs and copying the RAFs for each app into the stage2 directories
 //
 // Given a run command such as:
 //	rkt run --volume bind:/opt/tenant1/database \
@@ -18,14 +20,17 @@ package main
 //		example.com/ourapp-1.0.0 \
 //		example.com/logbackup-1.0.0
 //
-// the container doc generated will be compliant with the ACE spec.
+// the container manifest generated will be compliant with the ACE spec.
 //
 
 import (
+	"archive/tar"
+	"compress/gzip"
 	"encoding/json"
 	"errors"
 	"flag"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"os"
@@ -35,17 +40,20 @@ import (
 	// WARNING: here be dragons
 	// TODO(jonboulle): vendor this once the schema is stable
 	"github.com/containers/standard/schema"
+	"github.com/containers/standard/taf"
 )
 
 var (
 	fs = flag.NewFlagSet("rkt", flag.ExitOnError)
 
 	flagDir     string
+	flagStage1  string
 	flagVolumes volumeMap
 )
 
 func init() {
 	fs.StringVar(&flagDir, "dir", "", "directory in which to create container filesystem")
+	fs.StringVar(&flagStage1, "stage1", "./stage1", "path to stage1 binary")
 	fs.Var(&flagVolumes, "volume", "volumes to mount into the shared container environment")
 	flagVolumes = volumeMap{}
 }
@@ -84,7 +92,7 @@ func main() {
 		log.Fatalf("error creating UID: %v", err)
 	}
 
-	// - Generating the container document
+	// - Generating a Container Runtime Manifest
 	cm := &schema.ContainerManifest{
 		ACType: "ContainerManifest",
 		UID:    *cuid,
@@ -117,7 +125,7 @@ func main() {
 		log.Fatalf("error marshalling container manifest: %v", err)
 	}
 
-	// - Creating a directory for the container
+	// - Creating a filesystem for the container
 	if err := os.MkdirAll(dir, 0700); err != nil {
 		log.Fatalf("error creating directory: %v", err)
 	}
@@ -128,16 +136,55 @@ func main() {
 		log.Fatalf("error writing container manifest: %v", err)
 	}
 
-	// - Copying the stage1 into the container directory
-	// TODO(jonboulle): ???
-	fn = filepath.Join(dir, "stage1", "opt")
+	// - Setting up stage 1 and stage 2 directories in the filesystem
+	fn = filepath.Join(dir, "stage1", "opt", "stage2")
 	if err := os.MkdirAll(fn, 0700); err != nil {
 		log.Fatalf("error setting up stage1: %v", err)
 	}
 
-	// - Copying the RAFs for each app into the stage2 directories
+	// - Copying the stage1 binary into the container filesystem
+	in, err := os.Open(flagStage1)
+	if err != nil {
+		log.Fatalf("error loading stage1 binary: %v", err)
+	}
+	fn = filepath.Join(dir, "stage1", "init")
+	out, err := os.OpenFile(fn, os.O_CREATE|os.O_WRONLY, 0555)
+	if err != nil {
+		log.Fatalf("error opening stage1 init for writing: %v", err)
+	}
+	if _, err := io.Copy(out, in); err != nil {
+		log.Fatalf("error writing stage1 init: %v", err)
+	}
 
-	fmt.Printf("Wrote filesystem to %s\n", dir)
+	// - Fetching the TAFs for the specified applications
+	// (for now, we just assume they are local and unencrypted)
+
+	// - Unpacking the TAFs and copying the RAFs for each app into the stage2 directories
+	for _, name := range apps {
+		fn := name + ".tar.gz"
+		fh, err := os.Open(fn)
+		if err != nil {
+			log.Fatalf("error opening app: %v", err)
+		}
+		gz, err := gzip.NewReader(fh)
+		if err != nil {
+			log.Fatalf("error reading tarball: %v", err)
+		}
+		d := filepath.Join(dir, "stage1", "opt", "stage2", name)
+		err = os.MkdirAll(d, 0776)
+		if err != nil {
+			log.Fatalf("error creating app directory: %v", err)
+		}
+		if err := taf.ExtractTar(tar.NewReader(gz), d); err != nil {
+			log.Fatalf("error extracting TAF: %v", err)
+		}
+	}
+
+	log.Printf("Wrote filesystem to %s\n", dir)
+
+	if err := os.Chdir(dir); err != nil {
+		log.Fatalf("failed changing to dir: %v", err)
+	}
 }
 
 // newSchemaApp creates a new schema.App from a command-line name
