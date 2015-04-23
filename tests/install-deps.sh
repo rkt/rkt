808e9f9166815d6a2a5ae12ae4f2e01586dfe6f7
Alban Crequy <alban@endocode.com>
2015-04-23 10:38:50 +0200

    Semaphore: reduce dependencies
    The new Semaphore platform has most packages already installed.
    
    Use awk instead of gawk in functional tests: awk is more commonly
    installed than gawk.

--- tests/install-deps.sh
+++ tests/install-deps.sh
@@ -5,13 +5,14 @@
 if [ "${CI-}" == true ] ; then
 	# https://semaphoreci.com/
 	if [ "${SEMAPHORE-}" == true ] ; then
-		sudo apt-get update -qq
-		sudo apt-get install -y cpio realpath squashfs-tools
-		sudo apt-get install -y strace gdb libcap-ng-utils
-		sudo apt-get install -y coreutils # systemd needs a recent /bin/ln
-		sudo apt-get install -y gperf libcap-dev intltool # systemd deps
-		sudo apt-get install -y git # cloning a tag of systemd
-		sudo apt-get install -y gawk # used by TestEnv
+		# Most dependencies are already installed on Semaphore.
+		# Here we can install any missing dependencies. Whenever
+		# Semaphore installs more dependencies on their platform,
+		# they should be removed from here to save time.
+
+		#sudo apt-get update -qq
+		#sudo apt-get install -y packagename
+		:
 	fi
 
 	# https://circleci.com/
