e6ac97f26af8063e7e45c5ca06bd561bf7b0d6a2
Djalal Harouni <djalal@endocode.com>
2015-07-03 15:34:29 +0100

    build: In autogen, switch to sh instead and print a status message
    Let the user know that the autogen was executed successfully.
    
    Signed-off-by: Djalal Harouni <djalal@endocode.com>

--- autogen.sh
+++ autogen.sh
@@ -1,2 +1,21 @@
-#!/bin/bash -e
+#!/bin/sh
+
+set -e
+
+topdir=$(dirname $0)
+cd $topdir
+
 autoreconf --install --warnings=all --force
+
+args=""
+if [ -f "$topdir/.config.args" ]; then
+	args="$args $(cat $topdir/.config.args)"
+fi
+
+echo
+echo "----------------------------------------------------------------"
+echo "Initialized build system. For a common configuration please run:"
+echo "----------------------------------------------------------------"
+echo
+echo "$topdir/configure --with-stage1=coreos $args"
+echo
