b4f882f09b31243ef1e96ad130d3f87c0dbd536e
Alban Crequy <alban@endocode.com>
2015-07-07 14:15:01 +0200

    isolator functional tests: Fix aci path
    patchTestACI() now returns the new file path and callers are expected to
    use that instead of guessing which file to use.

--- tests/rkt_app_isolator_test.go
+++ tests/rkt_app_isolator_test.go
@@ -33,21 +33,17 @@ const (
 var memoryTest = struct {
 	testName     string
 	aciBuildArgs []string
-	rktArgs      string
 }{
 	`Check memory isolator`,
 	[]string{"--exec=/inspect --print-memorylimit"},
-	`--insecure-skip-verify run --mds-register=false rkt-inspect-isolators.aci`,
 }
 
 var cpuTest = struct {
 	testName     string
 	aciBuildArgs []string
-	rktArgs      string
 }{
 	`Check CPU quota`,
 	[]string{"--exec=/inspect --print-cpuquota"},
-	`--insecure-skip-verify run --mds-register=false rkt-inspect-isolators.aci`,
 }
 
 func TestAppIsolatorMemory(t *testing.T) {
@@ -64,7 +60,7 @@ func TestAppIsolatorMemory(t *testing.T) {
 	aciFileName := patchTestACI("rkt-inspect-isolators.aci", memoryTest.aciBuildArgs...)
 	defer os.Remove(aciFileName)
 
-	rktCmd := fmt.Sprintf("%s %s", ctx.cmd(), memoryTest.rktArgs)
+	rktCmd := fmt.Sprintf("%s --insecure-skip-verify run --mds-register=false %s", ctx.cmd(), aciFileName)
 	t.Logf("Command: %v", rktCmd)
 	child, err := gexpect.Spawn(rktCmd)
 	if err != nil {
@@ -95,7 +91,7 @@ func TestAppIsolatorCPU(t *testing.T) {
 	aciFileName := patchTestACI("rkt-inspect-isolators.aci", cpuTest.aciBuildArgs...)
 	defer os.Remove(aciFileName)
 
-	rktCmd := fmt.Sprintf("%s %s", ctx.cmd(), cpuTest.rktArgs)
+	rktCmd := fmt.Sprintf("%s --insecure-skip-verify run --mds-register=false %s", ctx.cmd(), aciFileName)
 	t.Logf("Command: %v", rktCmd)
 	child, err := gexpect.Spawn(rktCmd)
 	if err != nil {
