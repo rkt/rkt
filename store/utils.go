9394ee203c61bc63c1862ada3fab7dc7b53e99bd
Simone Gotti <simone.gotti@gmail.com>
2015-07-30 18:08:53 +0200

    store: print error reporting name and labels in GetACI.
    Report provided name and labels in GetACI instead of a generic "aci not found"
    error if it doesn't find an ACI matching them.
    
    To get an ordered labels list, a new function labelsToString is added. It sorts
    by label name giving precedence to the "version", "os" and "arch" (in this
    order) labels.
    
    Perhaps this function, if deemed useful, should be ported to appc/spec.

--- store/utils.go
+++ store/utils.go
@@ -17,7 +17,10 @@ package store
 import (
 	"fmt"
 	"net/url"
+	"sort"
 	"strings"
+
+	"github.com/coreos/rkt/Godeps/_workspace/src/github.com/appc/spec/schema/types"
 )
 
 // blockTransform creates a path slice from the given string to use as a
@@ -40,4 +43,37 @@ func blockTransform(s string) []string {
 func parseAlways(s string) *url.URL {
 	u, _ := url.Parse(s)
 	return u
+}
+
+func getLabelPriority(name types.ACIdentifier) int {
+	labelsPriority := map[types.ACIdentifier]int{
+		"version": 0,
+		"os":      1,
+		"arch":    2,
+	}
+	if i, ok := labelsPriority[name]; ok {
+		return i
+	}
+	return len(labelsPriority) + 1
+}
+
+// labelsSlice implements sort.Interface for types.Labels.
+type labelsSlice types.Labels
+
+func (p labelsSlice) Len() int { return len(p) }
+func (p labelsSlice) Less(i, j int) bool {
+	return getLabelPriority(p[i].Name) < getLabelPriority(p[j].Name)
+}
+func (p labelsSlice) Swap(i, j int) { p[i], p[j] = p[j], p[i] }
+
+func labelsToString(inLabels types.Labels) string {
+	// take a copy to void changing the original slice
+	labels := append(types.Labels(nil), inLabels...)
+	sort.Sort(labelsSlice(labels))
+
+	out := []string{}
+	for _, l := range labels {
+		out = append(out, fmt.Sprintf("%q:%q", l.Name, l.Value))
+	}
+	return "[" + strings.Join(out, ", ") + "]"
 }
