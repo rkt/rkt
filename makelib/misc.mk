7c4cf8b91865efa28929e23ab8f8fe5bf073015f
Krzesimir Nowak <krzesimir@endocode.com>
2015-08-07 14:21:16 +0200

    build: Add make functions for undefining variables
    They can be used to undefine all the variables in given namespaces in
    a single line instead of creating long lists of empty value
    assignments at the end of mk files or functions. This will be a bit of
    a relieve, because there will be no need to keep those lists in sync
    with newly-added or just-removed variables.

--- makelib/misc.mk
+++ makelib/misc.mk
@@ -1,3 +1,110 @@
+# check if we have undefine feature (only in make >=3.82)
+ifneq ($(filter undefine,$(.FEATURES)),)
+
+# we have undefine
+define undef
+$(eval undefine $1)
+endef
+
+else
+
+# no undefine available, simply set the variable to empty value
+define undef
+$(eval $1 :=)
+endef
+
+endif
+
+# 1 - a list of variables to undefine
+#
+# Simply uses undefine directive on all passed variables.
+#
+# It does not check if variables are in any way special (like being
+# special make variables or else).
+#
+# Example: $(call undefine-variables-unchecked,VAR1 VAR2 VAR3)
+define undefine-variables-unchecked
+$(strip \
+	$(foreach v,$1, \
+		$(call undef,$v)))
+endef
+
+# 1 - a list of variable namespaces
+# 2 - a list of excluded variables
+#
+# Undefines all variables in all given namespaces (which basically
+# means variables with names prefixed with <namespace>_) except for
+# ones listed in a given exclusions list.
+#
+# It does not check if variables are in any way special (like being
+# special make variables or else).
+#
+# It is a bit of make-golf to avoid using variables. See
+# undefine-namespaces below, which has clearer code, is doing exactly
+# the same, but calls undefine-variables instead (which changes its
+# behaviour wrt. the origin of the variables).
+#
+# Example: $(call undefine-namespaces-unchecked,NS1 NS2 NS3,N1_KEEP_THIS N3_THIS_TOO)
+define undefine-namespaces-unchecked
+$(strip \
+	$(foreach ,x x, \
+		$(call undefine-variables-unchecked, \
+			$(filter-out $2, \
+				$(filter $(foreach n,$1,$n_%),$(.VARIABLES))))))
+endef
+
+# 1 - a list of variables to undefine
+#
+# Undefines those variables from a given list, which have origin
+# "file". If the origin of a variable is different, it is left
+# untouched.
+#
+# This function will bail out if any of the variables starts with a
+# dot or MAKE.
+#
+# Example: $(call undefine-variables,VAR1 VAR2 VAR3)
+define undefine-variables
+$(strip \
+	$(foreach p,.% MAKE%, \
+		$(eval _MISC_UV_FORBIDDEN_ := $(strip $(filter $p,$1))) \
+		$(if $(_MISC_UV_FORBIDDEN_), \
+			$(eval _MISC_UV_ERROR_ += Trying to undefine $(_MISC_UV_FORBIDDEN_) variables which match the forbidden pattern $p.))) \
+	$(if $(_MISC_UV_ERROR_), \
+		$(error $(_MISC_UV_ERROR_))) \
+	$(foreach v,$1, \
+		$(if $(filter-out file,$(origin $v)), \
+			$(eval _MISC_UV_EXCLUDES_ += $v))) \
+	$(eval _MISC_UV_VARS_ := $(filter-out $(_MISC_UV_EXCLUDES_), $1)) \
+	$(call undefine-variables-unchecked,$(_MISC_UV_VARS_)) \
+	$(call undefine-namespaces-unchecked,_MISC_UV))
+endef
+
+# 1 - a list of variable namespaces
+# 2 - a list of excluded variables
+#
+# Undefines those variables in all given namespaces (which basically
+# means variables with names prefixed with <namespace>_), which have
+# origin "file". If the origin of the variable is different or the
+# variable is a part of exclusions list, it is left untouched.
+#
+# This function will bail out if any of the variables starts with a
+# dot or MAKE.
+#
+# The function performs the action twice - sometimes defined variables
+# are not listed in .VARIABLES list initially, but they do show up
+# there after first iteration, so we can remove them then. It is
+# likely a make bug.
+#
+# Example: $(call undefine-namespaces,NS1 NS2 NS3,N1_KEEP_THIS N3_THIS_TOO)
+define undefine-namespaces
+$(strip \
+	$(foreach ,x x, \
+		$(eval _MISC_UN_VARS_ := $(filter $(foreach n,$1,$n_%),$(.VARIABLES))) \
+		$(eval _MISC_UN_VARS_ := $(filter-out $2,$(_MISC_UN_VARS_))) \
+		$(call undefine-variables,$(_MISC_UN_VARS_)) \
+		$(call undefine-namespaces-unchecked,_MISC_UN)))
+endef
+
 define multi-subst
 $(eval _LIB_MK_TMP_MSUBST_VAL_ := $(strip $3)) \
 $(eval $(foreach s,$1, \
