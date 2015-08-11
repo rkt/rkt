7835ecb533da9dbe64976948dc64b3fe98ad632a
Krzesimir Nowak <krzesimir@endocode.com>
2015-08-11 11:22:06 +0200

    build: Cleanup and undefine namespaces in misc.mk

--- makelib/misc.mk
+++ makelib/misc.mk
@@ -106,21 +106,22 @@ $(strip \
 endef
 
 define multi-subst
-$(eval _LIB_MK_TMP_MSUBST_VAL_ := $(strip $3)) \
-$(eval $(foreach s,$1, \
-        $(eval _LIB_MK_TMP_MSUBST_VAL_ := $(subst $s,$2,$(_LIB_MK_TMP_MSUBST_VAL_))))) \
-$(strip $(_LIB_MK_TMP_MSUBST_VAL_)) \
-$(eval _LIB_MK_TMP_MSUBST_VAL_ :=)
+$(strip \
+	$(eval _MISC_MS_TMP_ := $(strip $3)) \
+	$(eval $(foreach s,$1, \
+		$(eval _MISC_MS_TMP_ := $(subst $s,$2,$(_MISC_MS_TMP_))))) \
+	$(_MISC_MS_TMP_) \
+	$(call undefine-namespaces,_MISC_MS))
 endef
 
 # When updating replaced chars here, remember to update them in
 # libdepsgen.pm in escape_path sub.
 define escape-for-file
-$(strip $(call multi-subst,- / . : +,_,$1))
+$(call multi-subst,- / . : +,_,$1)
 endef
 
 define path-to-file-with-suffix
-$(strip $(call escape-for-file,$1).$2)
+$(call escape-for-file,$1).$2
 endef
 
 define stamp-file
@@ -133,9 +134,10 @@ endef
 # stamp.
 # Example: $(call setup-custom-stamp-file,FOO_STAMP,/some_suffix)
 define setup-custom-stamp-file
-$(eval $1 := $(call stamp-file,$2)) \
-$(eval $($1): | $$(call to-dir,$($1))) \
-$(eval CLEAN_FILES += $($1))
+$(strip \
+	$(eval $1 := $(call stamp-file,$2)) \
+	$(eval $($1): | $$(call to-dir,$($1))) \
+	$(eval CLEAN_FILES += $($1)))
 endef
 
 # Generates a stamp filename and assigns it to passed variable
@@ -152,32 +154,14 @@ $(DEPSDIR)/$(call path-to-file-with-suffix,$1,dep.mk)
 endef
 
 define setup-custom-dep-file
-$(eval $1 := $(call dep-file,$2)) \
-$(eval $($1): | $$(call to-dir,$($1))) \
-$(eval CLEAN_FILES += $($1))
+$(strip \
+	$(eval $1 := $(call dep-file,$2)) \
+	$(eval $($1): | $$(call to-dir,$($1))) \
+	$(eval CLEAN_FILES += $($1)))
 endef
 
 define setup-dep-file
 $(eval $(call setup-custom-dep-file,$1,$(MK_PATH)$2))
-endef
-
-# 1 - test packages
-# 2 - go list format item
-# 3 - excluded directories
-define go-find-directories-unstripped
-$(eval _TST_ESCAPED_SRCDIR_ := $(MK_TOPLEVEL_ABS_SRCDIR)) \
-$(eval _TST_ESCAPED_SRCDIR_ := $(subst .,\.,$(_TST_ESCAPED_SRCDIR_))) \
-$(eval _TST_ESCAPED_SRCDIR_ := $(subst /,\/,$(_TST_ESCAPED_SRCDIR_))) \
-$(eval _TST_SPACE_ := ) \
-$(eval _TST_SPACE_ += ) \
-$(eval _TST_FILES_ := $(shell $(GO_ENV) "$(GO)" list -f '{{.ImportPath}} {{.$2}}' $1 | \
-        grep --invert-match '\[\]' | \
-        sed -e 's/.*$(_TST_ESCAPED_SRCDIR_)\///' -e 's/[[:space:]]*\[.*\]$$//' \
-        $(if $3,| grep --invert-match '^\($(subst $(_TST_SPACE_),\|,$3)\)'))) \
-$(_TST_FILES_) \
-$(eval _TST_ESCAPED_SRCDIR_ :=) \
-$(eval _TST_SPACE_ :=) \
-$(eval _TST_FILES_ :=)
 endef
 
 # Returns all not-excluded directories inside $REPO_PATH that have
@@ -187,7 +171,18 @@ endef
 # 3 - space-separated list of excluded directories
 # Example: $(call go-find-directories,./...,TestGoFiles,tests)
 define go-find-directories
-$(strip $(call go-find-directories-unstripped,$1,$2,$3))
+$(strip \
+	$(eval _MISC_GFD_ESCAPED_SRCDIR := $(MK_TOPLEVEL_ABS_SRCDIR)) \
+	$(eval _MISC_GFD_ESCAPED_SRCDIR := $(subst .,\.,$(_MISC_GFD_ESCAPED_SRCDIR))) \
+	$(eval _MISC_GFD_ESCAPED_SRCDIR := $(subst /,\/,$(_MISC_GFD_ESCAPED_SRCDIR))) \
+	$(eval _MISC_GFD_SPACE_ :=) \
+	$(eval _MISC_GFD_SPACE_ +=) \
+	$(eval _MISC_GFD_FILES_ := $(shell $(GO_ENV) "$(GO)" list -f '{{.ImportPath}} {{.$2}}' $1 | \
+		grep --invert-match '\[\]' | \
+		sed -e 's/.*$(_MISC_GFD_ESCAPED_SRCDIR)\///' -e 's/[[:space:]]*\[.*\]$$//' \
+		$(if $3,| grep --invert-match '^\($(subst $(_MISC_GFD_SPACE_),\|,$3)\)'))) \
+	$(_MISC_GFD_FILES_) \
+	$(call undefine-namespaces,_MISC_GFD))
 endef
 
 # Returns 1 if both parameters are equal, otherwise returns empty
@@ -195,10 +190,9 @@ endef
 # Example: is_a_equal_to_b := $(if $(call equal,a,b),yes,no)
 define equal
 $(strip \
-        $(eval _EQ_TMP_ := $(shell expr '$1' = '$2')) \
-        $(filter $(_EQ_TMP_),1) \
-        $(eval _EQ_TMP_ :=) \
-)
+        $(eval _MISC_EQ_TMP_ := $(shell expr '$1' = '$2')) \
+        $(filter $(_MISC_EQ_TMP_),1) \
+        $(call undefine-namespaces,_MISC_EQ))
 endef
 
 # Returns a string with all backslashes and double quotes escaped and
