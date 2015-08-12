e9037af0a7d42aa8b5577817257a2dfd29db6d88
Stefan Junker <mail@stefanjunker.de>
2015-08-12 15:30:03 +0200

    testutils: extend iputils with iface discovery

--- tests/testutils/iputils.go
+++ tests/testutils/iputils.go
@@ -88,3 +88,49 @@ func GetGWv4(iface string) (string, error) {
 func GetGWv6(iface string) (string, error) {
 	return GetGW(iface, netlink.FAMILY_V4)
 }
+
+func GetNonLoIfaceWithAddrs() (iface net.Interface, err error) {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return iface, err
+	}
+
+	for _, i := range ifaces {
+		if i.Flags&net.FlagLoopback == 0 {
+			ifaceNameLower := strings.ToLower(i.Name)
+			// Don't use rkt's interfaces
+			if strings.HasSuffix(ifaceNameLower, "cni") ||
+				strings.HasSuffix(ifaceNameLower, "veth") {
+				continue
+			}
+			addrs, err := i.Addrs()
+			if err != nil {
+				return iface, fmt.Errorf("Cannot get IPV4 address for interface %v: %v", i.Name, err)
+			}
+			if len(addrs) > 0 {
+				iface = i
+				break
+			}
+		}
+	}
+	return iface, err
+}
+
+func GetNonLoIfaceIPv4() (string, error) {
+	iface, err := GetNonLoIfaceWithAddrs()
+	if err != nil {
+		return "", fmt.Errorf("Error while getting non-lo host interface: %v\n", err)
+	}
+	if iface.Name == "" {
+		return "", nil
+	}
+
+	ifaceIPsv4, err := GetIPsv4(iface.Name)
+	if err != nil {
+		return "", fmt.Errorf("Cannot get IPV4 address for interface %v: %v", iface.Name, err)
+	}
+	if len(ifaceIPsv4) == 0 {
+		return "", nil
+	}
+	return ifaceIPsv4[0], nil
+}
