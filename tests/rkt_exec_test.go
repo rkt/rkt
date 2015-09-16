ff0bb5d544c64c2dcb755014e2e6e89888807b3c
Derek Gonyeo <derek.gonyeo@coreos.com>
2015-09-16 11:41:40 -0700

    stage0: Added test for prepared --exec
    Added a new test to make sure that the --exec flag on the prepare
    command works as intended.

--- tests/rkt_exec_test.go
+++ tests/rkt_exec_test.go
@@ -17,8 +17,10 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 	"testing"
 
+	"github.com/coreos/rkt/Godeps/_workspace/src/github.com/appc/spec/schema/types"
 	"github.com/coreos/rkt/Godeps/_workspace/src/github.com/steveeJ/gexpect"
 )
 
@@ -28,7 +30,7 @@ func TestRunOverrideExec(t *testing.T) {
 	ctx := newRktRunCtx()
 	defer ctx.cleanup()
 
-	for i, tt := range []struct {
+	for _, tt := range []struct {
 		rktCmd       string
 		expectedLine string
 	}{
@@ -43,17 +45,70 @@ func TestRunOverrideExec(t *testing.T) {
 			expectedLine: "inspect execed as: /inspect-link",
 		},
 	} {
-		child, err := gexpect.Spawn(tt.rktCmd)
-		if err != nil {
-			t.Fatalf("%d: cannot exec rkt: %v", i, err)
-		}
-
-		if err = expectWithOutput(child, tt.expectedLine); err != nil {
-			t.Fatalf("%d: didn't receive expected output %q: %v", i, tt.expectedLine, err)
-		}
-
-		if err = child.Wait(); err != nil {
-			t.Fatalf("%d: rkt didn't terminate correctly: %v", i, err)
-		}
+		runRktAndCheckOutput(t, tt.rktCmd, tt.expectedLine)
 	}
+}
+
+func TestRunPreparedOverrideExec(t *testing.T) {
+	execImage := patchTestACI("rkt-exec-override.aci", "--exec=/inspect")
+	defer os.Remove(execImage)
+	ctx := newRktRunCtx()
+	defer ctx.cleanup()
+
+	var rktCmd, uuid, expected string
+
+	// Sanity check - make sure no --exec override prints the expected exec invocation
+	rktCmd = fmt.Sprintf("%s prepare --insecure-skip-verify %s -- --print-exec", ctx.cmd(), execImage)
+	uuid = runRktAndGetUUID(t, rktCmd)
+
+	rktCmd = fmt.Sprintf("%s run-prepared --mds-register=false %s", ctx.cmd(), uuid)
+	expected = "inspect execed as: /inspect"
+	runRktAndCheckOutput(t, rktCmd, expected)
+
+	// Now test overriding the entrypoint (which is a symlink to /inspect so should behave identically)
+	rktCmd = fmt.Sprintf("%s prepare --insecure-skip-verify %s --exec /inspect-link -- --print-exec", ctx.cmd(), execImage)
+	uuid = runRktAndGetUUID(t, rktCmd)
+
+	rktCmd = fmt.Sprintf("%s run-prepared --mds-register=false %s", ctx.cmd(), uuid)
+	expected = "inspect execed as: /inspect-link"
+	runRktAndCheckOutput(t, rktCmd, expected)
+}
+
+func runRktAndCheckOutput(t *testing.T, rktCmd, expectedLine string) {
+	child, err := gexpect.Spawn(rktCmd)
+	if err != nil {
+		t.Fatalf("cannot exec rkt: %v", err)
+	}
+
+	if err = expectWithOutput(child, expectedLine); err != nil {
+		t.Fatalf("didn't receive expected output %q: %v", expectedLine, err)
+	}
+
+	if err = child.Wait(); err != nil {
+		t.Fatalf("rkt didn't terminate correctly: %v", err)
+	}
+}
+
+func runRktAndGetUUID(t *testing.T, rktCmd string) string {
+	child, err := gexpect.Spawn(rktCmd)
+	if err != nil {
+		t.Fatalf("cannot exec rkt: %v", err)
+	}
+
+	result, out, err := expectRegexWithOutput(child, "\n[0-9a-f-]{36}")
+	if err != nil || len(result) != 1 {
+		t.Fatalf("Error: %v\nOutput: %v", err, out)
+	}
+
+	if err = child.Wait(); err != nil {
+		t.Fatalf("rkt didn't terminate correctly: %v", err)
+	}
+
+	podIDStr := strings.TrimSpace(result[0])
+	podID, err := types.NewUUID(podIDStr)
+	if err != nil {
+		t.Fatalf("%q is not a valid UUID: %v", podIDStr, err)
+	}
+
+	return podID.String()
 }
