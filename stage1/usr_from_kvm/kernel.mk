1bb5ba42762dd61b03acc9c222c3bca4cc8c4ddd
Krzesimir Nowak <krzesimir@endocode.com>
2015-09-16 16:16:50 +0200

    build: Cleanups in kernel.mk
    The bzImage file might be created somewhere during the make run. To
    ensure that the build has finished completely we make the stamp
    creation to be the last step in the rule. We will want all the created
    files to be there, so we can put them into filelist.
    
    This is a preparatory commit for adding filelist generation.

--- stage1/usr_from_kvm/kernel.mk
+++ stage1/usr_from_kvm/kernel.mk
@@ -10,12 +10,14 @@ KERNEL_URL := https://www.kernel.org/pub/linux/kernel/v4.x/$(KERNEL_TARBALL)
 KERNEL_MAKEFILE := $(KERNEL_SRCDIR)/Makefile
 KERNEL_STUFFDIR := $(MK_SRCDIR)/kernel
 KERNEL_SRC_CONFIG := $(KERNEL_STUFFDIR)/cutdown-config
-KERNEL_PATCHES := $(abspath $(KERNEL_STUFFDIR)/patches/*.patch)
+KERNEL_PATCHESDIR := $(KERNEL_STUFFDIR)/patches
+KERNEL_PATCHES := $(abspath $(KERNEL_PATCHESDIR)/*.patch)
 KERNEL_BUILD_CONFIG := $(KERNEL_BUILDDIR)/.config
 KERNEL_BZIMAGE := $(KERNEL_BUILDDIR)/arch/x86/boot/bzImage
 KERNEL_ACI_BZIMAGE := $(ACIROOTFSDIR)/bzImage
 
 $(call setup-stamp-file,KERNEL_STAMP,/build_kernel)
+$(call setup-stamp-file,KERNEL_BZIMAGE_STAMP,/bzimage)
 $(call setup-stamp-file,KERNEL_PATCH_STAMP,/patch_kernel)
 $(call setup-dep-file,KERNEL_PATCHES_DEPMK)
 
@@ -28,10 +30,17 @@ UFK_STAMPS += $(KERNEL_STAMP)
 $(KERNEL_STAMP): $(KERNEL_ACI_BZIMAGE)
 	touch "$@"
 
+# $(KERNEL_ACI_BZIMAGE) has a dependency on $(KERNEL_BZIMAGE), which
+# is actually provided by $(KERNEL_BZIMAGE_STAMP)
+$(KERNEL_BZIMAGE): $(KERNEL_BZIMAGE_STAMP)
+
+# This stamp is to make sure that building linux kernel has finished.
 $(call forward-vars,$(KERNEL_BZIMAGE), \
 	MAKE KERNEL_SRCDIR KERNEL_BUILDDIR)
-$(KERNEL_BZIMAGE): $(KERNEL_BUILD_CONFIG) $(KERNEL_PATCH_STAMP)
-	$(MAKE) -C "$(KERNEL_SRCDIR)" O="$(KERNEL_BUILDDIR)" bzImage
+$(KERNEL_BZIMAGE_STAMP): $(KERNEL_BUILD_CONFIG) $(KERNEL_PATCH_STAMP)
+	set -e; \
+	$(MAKE) -C "$(KERNEL_SRCDIR)" O="$(KERNEL_BUILDDIR)" bzImage; \
+	touch "$@"
 
 -include $(KERNEL_PATCHES_DEPMK)
 $(call forward-vars,$(KERNEL_PATCH_STAMP), \
@@ -48,7 +57,8 @@ $(KERNEL_PATCH_STAMP): $(KERNEL_MAKEFILE) $(DEPSGENTOOL_STAMP)
 $(call forward-vars,$(KERNEL_MAKEFILE), \
 	KERNEL_SRCDIR KERNEL_TMP)
 $(KERNEL_MAKEFILE): $(KERNEL_TARGET_FILE)
-	rm -rf "$(KERNEL_SRCDIR)"
+	set -e; \
+	rm -rf "$(KERNEL_SRCDIR)"; \
 	tar --extract --xz --touch --file="$<" --directory="$(KERNEL_TMP)"
 
 $(call forward-vars,$(KERNEL_TARGET_FILE), \
