993b1ed4494aef904c603267338745942c8dce4b
Krzesimir Nowak <krzesimir@endocode.com>
2015-09-17 16:07:39 +0200

    units: Make default.target a symlink
    The current default.target becomes default-rkt.target to which
    default.target links. This makes it consistent with what default
    systemd install provides (default.target is a symlink there, too).
    
    Makes cleaning easier.

--- stage1/units/units.mk
+++ stage1/units/units.mk
@@ -7,7 +7,7 @@ LOCAL_UNITDIR_RESTS := \
 LOCAL_UNITDIR := $(LOCAL_UNITDIR_BASE)/$(LOCAL_UNITDIR_REST)
 LOCAL_UNIT_DIRS := $(foreach d,$(LOCAL_UNITDIR_RESTS),$(LOCAL_UNITDIR_BASE)/$d)
 LOCAL_UNIT_FILES := \
-        $(MK_SRCDIR)/units/default.target \
+        $(MK_SRCDIR)/units/default-rkt.target \
         $(MK_SRCDIR)/units/exit-watcher.service \
         $(MK_SRCDIR)/units/local-fs.target \
         $(MK_SRCDIR)/units/reaper.service \
@@ -32,12 +32,16 @@ $(foreach u,$(LOCAL_UNIT_FILES), \
         $(eval LOCAL_ACI_UNIT_FILES += $(_UNITS_MK_ACI_UNIT_)) \
         $(eval LOCAL_INSTALL_TRIPLETS += $u:$(_UNITS_MK_ACI_UNIT_):0644))
 
+$(call forward-vars,$(LOCAL_STAMP), \
+	LOCAL_UNITDIR)
 $(LOCAL_STAMP): $(LOCAL_ACI_UNIT_FILES) | $(LOCAL_UNIT_DIRS) $(LOCAL_CTRL_ALT_DEL)
+	ln -sf default-rkt.target "$(LOCAL_UNITDIR)/default.target"
 	touch "$@"
 
 STAGE1_INSTALL_FILES += $(LOCAL_INSTALL_TRIPLETS)
 STAGE1_INSTALL_SYMLINKS += $(LOCAL_UNIT_SYMLINKS)
 STAGE1_INSTALL_DIRS += $(foreach d,$(LOCAL_UNITDIR_RESTS),$(foreach d2,$(call dir-chain,$(LOCAL_UNITDIR_BASE),$d),$(d2):0755))
 STAGE1_STAMPS += $(LOCAL_STAMP)
+CLEAN_SYMLINKS += $(LOCAL_UNITDIR)/default.target
 
 $(call undefine-namespaces,LOCAL _UNITS_MK)
