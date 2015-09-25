0e723864248b257f723893bd7390471cb5a0d3e2
Vito Caputo <vito.caputo@coreos.com>
2015-09-24 23:48:18 -0700

    stage1/diagexec: support comma-separated list of gids w/setgroups()

--- stage1/diagexec/diagexec.c
+++ stage1/diagexec/diagexec.c
@@ -15,6 +15,7 @@
 #define _GNU_SOURCE
 #include <errno.h>
 #include <fcntl.h>
+#include <grp.h>
 #include <inttypes.h>
 #include <limits.h>
 #include <stdint.h>
@@ -210,6 +211,49 @@ static void load_env(const char *env, const char **keep_env)
 	}
 }
 
+/* Parse a comma-separated list of numeric gids from str, returns an malloc'd
+ * array of gids in *gids_p with the number of elements in *n_gids_p.
+ */
+static void parse_gids(const char *str, size_t *n_gids_p, gid_t **gids_p)
+{
+	char	c = ',', last_c;
+	int	i, n_gids = 0, done = 0;
+	gid_t	gid = 0;
+	gid_t	*gids = NULL;
+
+	for(i = 0; !done; i++) {
+		last_c = c;
+		switch(c = str[i]) {
+		case '0' ... '9':
+			gid *= 10;
+			gid += c - '0';
+			break;
+
+		case '\0':
+			done = 1;
+			/* fallthrough */
+		case ',':
+			exit_if(last_c == ',',
+				"Gids contains an empty gid: \"%s\"", str);
+			pexit_if((gids = realloc(gids, sizeof(*gids) * (n_gids + 1))) == NULL,
+				"Unable to allocate gids: \"%s\"", str);
+			gids[n_gids++] = gid;
+			gid = 0;
+			break;
+
+		default:
+			exit_if(1,
+				"Gids contains invalid input (%c): \"%s\"",
+				c, str);
+		}
+	}
+
+	exit_if(!n_gids, "At least one gid is required, got: \"%s\"", str);
+
+	*gids_p = gids;
+	*n_gids_p = n_gids;
+}
+
 int main(int argc, char *argv[])
 {
 	/* We need to keep these env variables since systemd uses them for socket
@@ -221,12 +265,14 @@ int main(int argc, char *argv[])
 		NULL
 	};
 
-	const char *root, *cwd, *env, *uid_str, *gid_str, *exe;
-	char **args;
-	uid_t uid;
-	gid_t gid;
+	const char	*root, *cwd, *env, *uid_str, *gid_str, *exe;
+	char		**args;
+	uid_t		uid;
+	gid_t		*gids;
+	size_t		n_gids;
+
 	exit_if(argc < 7,
-		"Usage: %s /path/to/root /work/directory /env/file uid gid /to/exec [args ...]", argv[0]);
+		"Usage: %s /path/to/root /work/directory /env/file uid gid[,gid...] /to/exec [args ...]", argv[0]);
 
 	root = argv[1];
 	cwd = argv[2];
@@ -234,16 +280,20 @@ int main(int argc, char *argv[])
 	uid_str = argv[4];
 	uid = atoi(uid_str);
 	gid_str = argv[5];
-	gid = atoi(gid_str);
 	args = &argv[6];
 	exe = args[0];
 
+	parse_gids(gid_str, &n_gids, &gids);
 	load_env(env, keep_env);
 
 	pexit_if(chroot(root) == -1, "Chroot \"%s\" failed", root);
 	pexit_if(chdir(cwd) == -1, "Chdir \"%s\" failed", cwd);
-	pexit_if(gid > 0 && setresgid(gid, gid, gid) == -1, "Setresgid \"%s\" failed", gid_str);
-	pexit_if(uid > 0 && setresuid(uid, uid, uid) == -1, "Setresuid \"%s\" failed", uid_str);
+	pexit_if(gids[0] > 0 && setresgid(gids[0], gids[0], gids[0]) == -1,
+		"Setresgid \"%s\" failed", gid_str);
+	pexit_if(n_gids > 1 && setgroups(n_gids - 1, &gids[1]) == -1,
+		"Setgroups \"%s\" failed", gid_str);
+	pexit_if(uid > 0 && setresuid(uid, uid, uid) == -1,
+		"Setresuid \"%s\" failed", uid_str);
 
 	/* XXX(vc): note that since execvp() is happening post-chroot, the
 	 * app's environment settings correctly affect the PATH search.
