0c2207686a9dd66cd1219b061da09d1ca2f0edc7
Iago López Galeiras <iaguis@gmail.com>
2015-10-06 09:47:55 +0200

    Godeps: bump goaci

--- Godeps/_workspace/src/github.com/appc/goaci/proj2aci/cmake.go
+++ Godeps/_workspace/src/github.com/appc/goaci/proj2aci/cmake.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/coreos/rkt/Godeps/_workspace/src/github.com/appc/spec/schema"
 	"github.com/coreos/rkt/Godeps/_workspace/src/github.com/appc/spec/schema/types"
+
 	"github.com/coreos/rkt/Godeps/_workspace/src/golang.org/x/tools/go/vcs"
 )
 
