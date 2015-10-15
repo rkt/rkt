b0007ad0e4d3abe09a63d289f97934a79ab020d4
Krzesimir Nowak <qdlacz@gmail.com>
2015-10-15 13:05:02 +0200

    build: Fix make clean for kvm flavor
    We have to remove both /var and /var/run directories and the enter
    symlink.

--- stage1/usr_from_kvm/files.mk
+++ stage1/usr_from_kvm/files.mk
@@ -1,6 +1,9 @@
 $(call setup-stamp-file,UFKF_STAMP)
 UFKF_DIR := $(MK_SRCDIR)/files
-UFKF_VAR_RUN := $(ACIROOTFSDIR)/var/run
+UFKF_BASE := $(ACIROOTFSDIR)
+UFKF_REST := var/run
+UFKF_DIR_CHAIN := $(call dir-chain,$(UFKF_BASE),$(UFKF_REST))
+UFKF_VAR_RUN := $(UFKF_BASE)/$(UFKF_REST)
 
 UFKF_ACI_FILES := \
 	$(ACIROOTFSDIR)/etc/passwd \
@@ -17,8 +20,7 @@ STAGE1_STAMPS += $(UFKF_STAMP)
 
 STAGE1_INSTALL_FILES += $(join $(addsuffix :,$(UFKF_SRC_FILES)),$(addsuffix :0644,$(UFKF_ACI_FILES)))
 STAGE1_INSTALL_DIRS += \
-	$(addsuffix :0755,$(sort $(call to-dir,$(UFKF_ACI_FILES)))) \
-	$(UFKF_VAR_RUN):0755
+	$(addsuffix :0755,$(UFKF_DIR_CHAIN) $(sort $(call to-dir,$(UFKF_ACI_FILES))))
 
 $(UFKF_STAMP): $(UFKF_ACI_FILES) | $(UFKF_VAR_RUN)
 	touch "$@"
