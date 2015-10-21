e2dc43908da26e7c2745eed599f4dce7ab14ce8e
Krzesimir Nowak <qdlacz@gmail.com>
2015-10-21 13:02:22 +0200

    build: Print nicer output in usr_from_host

--- stage1/usr_from_host/usr_from_host.mk
+++ stage1/usr_from_host/usr_from_host.mk
@@ -3,6 +3,7 @@ $(call setup-stamp-file,UFH_STAMP)
 S1_RF_USR_STAMPS += $(UFH_STAMP)
 
 $(call generate-stamp-rule,$(UFH_STAMP),,$(S1_RF_ACIROOTFSDIR), \
+	$(call vb,v2,LN SF,host $(S1_RF_ACIROOTFSDIR)/flavor) \
 	ln -sf 'host' "$(S1_RF_ACIROOTFSDIR)/flavor")
 
 CLEAN_SYMLINKS += $(S1_RF_ACIROOTFSDIR)/flavor
