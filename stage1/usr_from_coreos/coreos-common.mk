740ec726dec1ca231341f43247b298aba3a3e3b1
Krzesimir Nowak <qdlacz@gmail.com>
2015-10-29 18:03:36 +0100

    build: Unconditionally try to remove downloaded pxe.img
    If rkt was configured without the --with-coreos-local-pxe-image-*
    flags and built, then after reconfiguring it to use some other local
    pxe.img, the downloaded one wouldn't be removed on make clean.

--- stage1/usr_from_coreos/coreos-common.mk
+++ stage1/usr_from_coreos/coreos-common.mk
@@ -6,17 +6,21 @@ ifeq ($(_CCN_INCLUDED_),)
 
 _CCN_INCLUDED_ := x
 
+$(call setup-tmp-dir,CCN_TMPDIR)
+
 CCN_SYSTEMD_VERSION := "v222"
 CCN_IMG_RELEASE := "794.1.0"
 CCN_IMG_URL := "http://alpha.release.core-os.net/amd64-usr/$(CCN_IMG_RELEASE)/coreos_production_pxe_image.cpio.gz"
+CCN_DOWNLOADED_PXE := $(CCN_TMPDIR)/pxe.img
+CLEAN_FILES += \
+	$(CCN_DOWNLOADED_PXE) \
+	$(CCN_DOWNLOADED_PXE).$(firstword $(shell echo -n $(CCN_IMG_URL) | md5sum)).sig
 
 ifneq ($(RKT_LOCAL_COREOS_PXE_IMAGE_SYSTEMD_VER),)
 
 CCN_SYSTEMD_VERSION := $(RKT_LOCAL_COREOS_PXE_IMAGE_SYSTEMD_VER)
 
 endif
-
-$(call setup-tmp-dir,CCN_TMPDIR)
 
 CCN_SQUASHFS_BASE := usr.squashfs
 CCN_SQUASHFS := $(CCN_TMPDIR)/$(CCN_SQUASHFS_BASE)
@@ -30,12 +34,9 @@ CCN_PXE := $(abspath $(RKT_LOCAL_COREOS_PXE_IMAGE_PATH))
 
 else
 
-# We are going to download pxe.img, so we need to clean it too.
+# We are using downloaded pxe.img
 
-CCN_PXE := $(CCN_TMPDIR)/pxe.img
-CLEAN_FILES += \
-	$(CCN_PXE) \
-	$(CCN_PXE).$(firstword $(shell echo -n $(CCN_IMG_URL) | md5sum)).sig
+CCN_PXE := $(CCN_DOWNLOADED_PXE)
 
 endif
 
@@ -48,15 +49,11 @@ $(CCN_SQUASHFS): $(CCN_PXE) | $(CCN_TMPDIR)
 	$(call vb,vt,EXTRACT,$(call vsp,$(CCN_PXE)) => $(call vsp,$@)) \
 	cd "$(CCN_TMPDIR)" && gzip --to-stdout --decompress "$(CCN_PXE)" | cpio $(call vl3,--quiet )--unconditional --extract "$(CCN_SQUASHFS_BASE)"
 
-ifeq ($(RKT_LOCAL_COREOS_PXE_IMAGE_PATH),)
-
-$(call forward-vars,$(CCN_PXE), \
+$(call forward-vars,$(CCN_DOWNLOADED_PXE), \
 	CCN_TMPDIR CCN_IMG_URL BASH_SHELL CCN_CACHE_SH)
-$(CCN_PXE): $(CCN_CACHE_SH) | $(CCN_TMPDIR)
+$(CCN_DOWNLOADED_PXE): $(CCN_CACHE_SH) | $(CCN_TMPDIR)
 	$(VQ) \
 	ITMP="$(CCN_TMPDIR)" IMG_URL="$(CCN_IMG_URL)" V="$(V)" $(BASH_SHELL) $(CCN_CACHE_SH)
-
-endif
 
 # Excluding CCN_SQUASHFS because other will want to know where we
 # placed the squashfs file, CCN_SYSTEMD_VERSION might be needed to
