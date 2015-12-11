c263c0738575c61acfea926794f1aea1bc426d29
Iago López Galeiras <iago@kinvolk.io>
2015-12-11 13:52:53 +0100

    pkg: fileutil: take into account hard links
    When calculating directory size.
    
    Also, rearrange the linux/unsupported go files.

--- pkg/fileutil/fileutil.go
+++ pkg/fileutil/fileutil.go
@@ -204,10 +204,20 @@ func TimeToTimespec(time time.Time) (ts syscall.Timespec) {
 
 // DirSize takes a path and returns its size in bytes
 func DirSize(path string) (int64, error) {
+	seenInode := make(map[uint64]struct{})
+
 	if _, err := os.Stat(path); err == nil {
 		var sz int64
-		err := filepath.Walk(path, func(_ string, info os.FileInfo, err error) error {
-			sz += info.Size()
+		err := filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
+			if hasHardLinks(info) {
+				ino := getInode(info)
+				if _, ok := seenInode[ino]; !ok {
+					seenInode[ino] = struct{}{}
+					sz += info.Size()
+				}
+			} else {
+				sz += info.Size()
+			}
 			return err
 		})
 		return sz, err
