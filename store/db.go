d6b3ea8ddb2ae9c7388f398276ffba50e3592f28
Yifan Gu <yifan.gu@coreos.com>
2015-12-17 16:36:30 -0800

    store: Make db thread-safe.

--- store/db.go
+++ store/db.go
@@ -19,6 +19,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"sync"
 
 	"github.com/coreos/rkt/pkg/lock"
 
@@ -29,9 +30,42 @@ const (
 	DbFilename = "ql.db"
 )
 
+// dbLock is used to guarantee both thread-safety and process-safety
+// for db access.
+type dbLock struct {
+	fl *lock.FileLock
+	// Although the FileLock already ensures thread safety, the Go runtime is unaware
+	// of this, and so Mutex is necessary to satisfy the Go race detector.
+	sync.Mutex
+}
+
+func newDBLock(dirPath string) (*dbLock, error) {
+	l, err := lock.NewLock(dirPath, lock.Dir)
+	if err != nil {
+		return nil, err
+	}
+	return &dbLock{fl: l}, nil
+}
+
+func (dl *dbLock) lock() error {
+	if err := dl.fl.ExclusiveLock(); err != nil {
+		return err
+	}
+	dl.Lock()
+	return nil
+}
+
+func (dl *dbLock) unlock() error {
+	if err := dl.fl.Unlock(); err != nil {
+		return err
+	}
+	dl.Unlock()
+	return nil
+}
+
 type DB struct {
 	dbdir string
-	lock  *lock.FileLock
+	dl    *dbLock
 	sqldb *sql.DB
 }
 
@@ -39,44 +73,45 @@ func NewDB(dbdir string) (*DB, error) {
 	if err := os.MkdirAll(dbdir, defaultPathPerm); err != nil {
 		return nil, err
 	}
-	return &DB{dbdir: dbdir}, nil
+
+	dl, err := newDBLock(dbdir)
+	if err != nil {
+		return nil, err
+	}
+
+	return &DB{dbdir: dbdir, dl: dl}, nil
 }
 
 func (db *DB) Open() error {
-	// take a lock on db dir
-	if db.lock != nil {
-		panic("cas db lock already gained")
-	}
-	dl, err := lock.ExclusiveLock(db.dbdir, lock.Dir)
-	if err != nil {
+	if err := db.dl.lock(); err != nil {
 		return err
 	}
-	db.lock = dl
 
 	sqldb, err := sql.Open("ql", filepath.Join(db.dbdir, DbFilename))
 	if err != nil {
-		dl.Close()
+		unlockErr := db.dl.unlock()
+		if unlockErr != nil {
+			err = fmt.Errorf("[%s, %s]", err, unlockErr)
+		}
 		return err
 	}
 	db.sqldb = sqldb
+
 	return nil
 }
 
 func (db *DB) Close() error {
-	if db.lock == nil {
-		panic("cas db, Close called without lock")
-	}
 	if db.sqldb == nil {
 		panic("cas db, Close called without an open sqldb")
 	}
+
 	if err := db.sqldb.Close(); err != nil {
 		return fmt.Errorf("cas db close failed: %v", err)
 	}
 	db.sqldb = nil
 
-	db.lock.Close()
-	db.lock = nil
-	return nil
+	// Don't close the flock as it will be reused.
+	return db.dl.unlock()
 }
 
 func (db *DB) Begin() (*sql.Tx, error) {
