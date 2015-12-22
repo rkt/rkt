25d8a76b6e360b45b0d3bc5383175fbfd1898e10
Krzesimir Nowak <krzesimir@kinvolk.io>
2015-12-22 15:46:02 +0100

    functional tests: Slice the ServeHTTP into pieces

--- tests/testutils/aci-server/server.go
+++ tests/testutils/aci-server/server.go
@@ -49,86 +49,73 @@ type serverHandler struct {
 }
 
 func (h *serverHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "GET":
-		// handled later
-	default:
+	if r.Method != "GET" {
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
+	if authOk := h.handleAuth(w, r); !authOk {
+		return
+	}
+	h.sendMsg(fmt.Sprintf("Trying to serve %q", r.URL.String()))
+	h.handleRequest(w, r)
+}
+
+func (h *serverHandler) handleAuth(w http.ResponseWriter, r *http.Request) bool {
 	switch h.auth {
 	case None:
 		// no auth to do.
+		return true
 	case Basic:
-		payload, httpErr := getAuthPayload(r, "Basic")
-		if httpErr != nil {
-			w.WriteHeader(httpErr.code)
-			h.sendMsg(fmt.Sprintf(`No "Authorization" header: %v`, httpErr.message))
-			return
-		}
-		creds, err := base64.StdEncoding.DecodeString(string(payload))
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			h.sendMsg(fmt.Sprintf(`Badly formed "Authorization" header`))
-			return
-		}
-		parts := strings.Split(string(creds), ":")
-		if len(parts) != 2 {
-			w.WriteHeader(http.StatusBadRequest)
-			h.sendMsg(fmt.Sprintf(`Badly formed "Authorization" header (2)`))
-			return
-		}
-		user := parts[0]
-		password := parts[1]
-		if user != "bar" || password != "baz" {
-			w.WriteHeader(http.StatusUnauthorized)
-			h.sendMsg(fmt.Sprintf("Bad credentials: %q", string(creds)))
-			return
-		}
+		return h.handleBasicAuth(w, r)
 	case Oauth:
-		payload, httpErr := getAuthPayload(r, "Bearer")
-		if httpErr != nil {
-			w.WriteHeader(httpErr.code)
-			h.sendMsg(fmt.Sprintf(`No "Authorization" header: %v`, httpErr.message))
-			return
-		}
-		if payload != "sometoken" {
-			w.WriteHeader(http.StatusUnauthorized)
-			h.sendMsg(fmt.Sprintf(`Bad token: %q`, payload))
-			return
-		}
+		return h.handleOauthAuth(w, r)
 	default:
 		panic("Woe is me!")
 	}
-	h.sendMsg(fmt.Sprintf("Trying to serve %q", r.URL.String()))
-	switch filepath.Base(r.URL.Path) {
-	case "/":
-		indexHTML := `<meta name="ac-discovery" content="localhost https://localhost/{name}.{ext}">`
-		w.Write([]byte(indexHTML))
-		h.sendMsg(fmt.Sprintf("    done."))
-	default:
-		path, ok := h.fileSet[filepath.Base(r.URL.Path)]
-		if ok {
-			contents, err := ioutil.ReadFile(path)
-			if err == nil {
-				w.Write(contents)
-				h.sendMsg(fmt.Sprintf("    done."))
-			} else {
-				h.sendMsg(fmt.Sprintf("  not found."))
-				w.WriteHeader(http.StatusNotFound)
-			}
-		} else {
-			h.sendMsg(fmt.Sprintf("  not found."))
-			w.WriteHeader(http.StatusNotFound)
-		}
-	}
 }
 
-func (h *serverHandler) sendMsg(msg string) {
-	select {
-	case h.msg <- msg:
-	default:
+func (h *serverHandler) handleBasicAuth(w http.ResponseWriter, r *http.Request) bool {
+	payload, httpErr := getAuthPayload(r, "Basic")
+	if httpErr != nil {
+		w.WriteHeader(httpErr.code)
+		h.sendMsg(fmt.Sprintf(`No "Authorization" header: %v`, httpErr.message))
+		return false
 	}
+	creds, err := base64.StdEncoding.DecodeString(string(payload))
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		h.sendMsg(`Badly formed "Authorization" header`)
+		return false
+	}
+	parts := strings.Split(string(creds), ":")
+	if len(parts) != 2 {
+		w.WriteHeader(http.StatusBadRequest)
+		h.sendMsg(`Badly formed "Authorization" header (2)`)
+		return false
+	}
+	user := parts[0]
+	password := parts[1]
+	if user != "bar" || password != "baz" {
+		w.WriteHeader(http.StatusUnauthorized)
+		h.sendMsg(fmt.Sprintf("Bad credentials: %q", string(creds)))
+		return false
+	}
+	return true
+}
+
+func (h *serverHandler) handleOauthAuth(w http.ResponseWriter, r *http.Request) bool {
+	payload, httpErr := getAuthPayload(r, "Bearer")
+	if httpErr != nil {
+		w.WriteHeader(httpErr.code)
+		h.sendMsg(fmt.Sprintf(`No "Authorization" header: %v`, httpErr.message))
+		return false
+	}
+	if payload != "sometoken" {
+		w.WriteHeader(http.StatusUnauthorized)
+		h.sendMsg(fmt.Sprintf(`Bad token: %q`, payload))
+		return false
+	}
+	return true
 }
 
 func getAuthPayload(r *http.Request, authType string) (string, *httpError) {
@@ -156,6 +143,50 @@ func getAuthPayload(r *http.Request, authType string) (string, *httpError) {
 		return "", err
 	}
 	return parts[1], nil
+}
+
+func (h *serverHandler) handleRequest(w http.ResponseWriter, r *http.Request) {
+	path := filepath.Base(r.URL.Path)
+	switch path {
+	case "/":
+		h.sendAcDiscovery(w)
+		h.sendMsg("  done.")
+	default:
+		if found := h.handleFile(w, path); found {
+			h.sendMsg("  done.")
+		}
+	}
+}
+
+func (h *serverHandler) sendAcDiscovery(w http.ResponseWriter) {
+	// TODO(krnowak): When appc spec gets the discovery over
+	// custom port feature, possibly take it into account here
+	indexHTML := `<meta name="ac-discovery" content="localhost https://localhost/{name}.{ext}">`
+	w.Write([]byte(indexHTML))
+}
+
+func (h *serverHandler) handleFile(w http.ResponseWriter, reqPath string) bool {
+	path, ok := h.fileSet[reqPath]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		h.sendMsg("  not found.")
+		return false
+	}
+	contents, err := ioutil.ReadFile(path)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		h.sendMsg("  not found, but specified in fileset; bug?")
+		return false
+	}
+	w.Write(contents)
+	return true
+}
+
+func (h *serverHandler) sendMsg(msg string) {
+	select {
+	case h.msg <- msg:
+	default:
+	}
 }
 
 type Server struct {
