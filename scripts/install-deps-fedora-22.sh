d4e8ce2c0471c1908c5565c69d3a7cfcadb941ad
Iago López Galeiras <iago@kinvolk.io>
2016-01-07 13:45:55 +0100

    build: add libacl dev dependencies to build rkt-in-rkt scripts
    We depend on libacl development headers but we forgot to update the
    scripts to build rkt in rkt.
    
    Also, update the one-liners mentioned in the documentation.

--- scripts/install-deps-fedora-22.sh
+++ scripts/install-deps-fedora-22.sh
@@ -2,4 +2,4 @@
 
 set -xe
 
-dnf install -y make gcc glibc-devel glibc-static cpio squashfs-tools gpg autoconf make automake golang file git wget tar xz patch bc hostname findutils openssl
+dnf install -y make gcc glibc-devel glibc-static cpio squashfs-tools gpg autoconf make automake golang file git wget tar xz patch bc hostname findutils openssl libacl-devel
