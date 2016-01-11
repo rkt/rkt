6df6bc3336d8f33b74805e31e94e74a906a29dfd
Alban Crequy <alban@kinvolk.io>
2016-01-11 15:14:46 +0100

    bash completion
    Based on an initial implementation from Iago.
    
    Fixes https://github.com/coreos/rkt/issues/1419

--- rkt/rkt.go
+++ rkt/rkt.go
@@ -35,6 +35,62 @@ const (
 	cliDescription = "rkt, the application container runner"
 
 	defaultDataDir = "/var/lib/rkt"
+
+	bash_completion_func = `__rkt_parse_image()
+{
+    local rkt_output
+    if rkt_output=$(rkt image list --no-legend 2>/dev/null); then
+        out=($(echo "${rkt_output}" | awk '{print $1}'))
+        COMPREPLY=( $( compgen -W "${out[*]}" -- "$cur" ) )
+    fi
+}
+
+__rkt_parse_list()
+{
+    local rkt_output
+    if rkt_output=$(rkt list --no-legend 2>/dev/null); then
+        if [[ -n "$1" ]]; then
+            out=($(echo "${rkt_output}" | grep ${1} | awk '{print $1}'))
+        else
+            out=($(echo "${rkt_output}" | awk '{print $1}'))
+        fi
+        COMPREPLY=( $( compgen -W "${out[*]}" -- "$cur" ) )
+    fi
+}
+
+__custom_func() {
+    case ${last_command} in
+        rkt_image_export | \
+        rkt_image_extract | \
+        rkt_image_cat-manifest | \
+        rkt_image_render | \
+        rkt_image_rm | \
+        rkt_run | \
+        rkt_prepare)
+            __rkt_parse_image
+            return
+            ;;
+        rkt_run-prepared)
+            __rkt_parse_list prepared
+            return
+            ;;
+        rkt_enter)
+            __rkt_parse_list running
+            return
+            ;;
+        rkt_rm)
+            __rkt_parse_list "prepare\|exited"
+            return
+            ;;
+        rkt_status)
+            __rkt_parse_list
+            return
+            ;;
+        *)
+            ;;
+    esac
+}
+`
 )
 
 type absDir string
@@ -84,6 +140,7 @@ var (
 var cmdRkt = &cobra.Command{
 	Use:   "rkt [command]",
 	Short: cliDescription,
+	BashCompletionFunction: bash_completion_func,
 }
 
 func init() {
@@ -134,6 +191,11 @@ func main() {
 
 	// Make help just show the usage
 	cmdRkt.SetHelpTemplate(`{{.UsageString}}`)
+
+	// // Uncomment to update rkt.bash
+	// stdout("Generating rkt.bash")
+	// cmdRkt.GenBashCompletionFile("dist/bash_completion/rkt.bash")
+	// os.Exit(0)
 
 	cmdRkt.Execute()
 	os.Exit(cmdExitCode)
