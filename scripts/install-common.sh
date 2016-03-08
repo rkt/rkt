1f4c81fd3bdf32d62b26ff9a2e7e6f46d5ca5ae5
Martin Polden <martin.polden@gmail.com>
2016-03-08 22:45:50 +0100

    scripts: Add bc to common dependencies

--- scripts/install-common.sh
+++ scripts/install-common.sh
@@ -4,7 +4,7 @@ set -e
 export DEBIAN_FRONTEND=noninteractive
 
 apt-get update
-apt-get install -y --no-install-recommends ca-certificates gcc libc6-dev make automake wget git coreutils cpio squashfs-tools realpath autoconf file libacl1-dev libtspi-dev
+apt-get install -y --no-install-recommends ca-certificates gcc libc6-dev make automake wget git coreutils cpio squashfs-tools realpath autoconf file libacl1-dev libtspi-dev bc
 
 ./scripts/install-go.sh
 . /etc/profile
