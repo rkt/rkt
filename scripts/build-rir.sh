69353251fc2312a153268974f8d660af161218a6
Sergiusz Urbaniak <sur@coreos.com>
2016-04-15 16:14:28 +0200

    release: fix build-rir.sh script
    Currently `build-rir.sh` inherits the environment during the build. This
    is unnecessary and can actually break the build on a custom host
    PATH environment.
    
    This fixes it.

--- scripts/build-rir.sh
+++ scripts/build-rir.sh
@@ -2,15 +2,14 @@
 
 set -xe
 
-SRC_DIR=${SRC_DIR:-$PWD}
-BUILDDIR=${BUILDDIR:-$PWD/build-rir}
-RKT_BUILDER_ACI=${RKT_BUILDER_ACI:-coreos.com/rkt/builder:v1.4.0+git}
+SRC_DIR="${SRC_DIR:-$PWD}"
+BUILDDIR="${BUILDDIR:-$PWD/build-rir}"
+RKT_BUILDER_ACI="${RKT_BUILDER_ACI:-coreos.com/rkt/builder:v1.4.0+git}"
 
 mkdir -p $BUILDDIR
 
 rkt run \
-    --inherit-env \
-    --volume src-dir,kind=host,source=$SRC_DIR \
-    --volume build-dir,kind=host,source=$BUILDDIR \
+    --volume src-dir,kind=host,source="${SRC_DIR}" \
+    --volume build-dir,kind=host,source="${BUILDDIR}" \
     --interactive \
     $RKT_BUILDER_ACI $@
