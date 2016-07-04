0ffb76b3180efa5ba573aee76d16155efc4a3601
Alessandro Puccetti <alessandro@kinvolk.io>
2016-07-04 15:10:40 +0200

    stage1: diagnostic functionality for rkt run
    After this patch, if the app exits with `ExecMainStatus == 203`, the app's reaper runs
    the diagnostic tool and prints the output on stdout. systemd sets `ExecMainstatus`
    to EXIT_EXEC (203) when execve() fails. References:
    Exit status: https://github.com/systemd/systemd/blob/v230/src/basic/exit-status.h#L47
    call execve(): https://github.com/systemd/systemd/blob/v230/src/core/execute.c#L1998
    
    Example:
    ```
    sudo build-rkt-1.9.1+git/target/bin/rkt run coreos.com/etcd:v2.0.10 --exec=/foo-not-there
    image: using image from file /usr/lib/rkt/stage1-images/stage1-coreos.aci
    image: using image from local store for image name coreos.com/etcd:v2.0.10
    networking: loading networks from /etc/rkt/net.d
    networking: loading network default with type ptp
    Error: Unable to open "/foo-not-there": No such file or directory
    ```
    Fixes https://github.com/coreos/rkt/issues/2506

--- stage1/enterexec/enterexec.mk
+++ stage1/enterexec/enterexec.mk
@@ -1,3 +1,5 @@
-ASSCB_EXTRA_HEADERS := elf.h
+ASSCB_EXTRA_CFLAGS := -I$(MK_SRCDIR)/../diagnostic
+ASSCB_EXTRA_HEADERS := ../diagnostic/diagnostic-util.h
+ASSCB_EXTRA_SOURCES := ../diagnostic/diagnostic-util.c
 
 include stage1/makelib/aci_simple_static_c_bin.mk
