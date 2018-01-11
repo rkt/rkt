e3fa3d83cb44588909e2920ece7e0b962aecd4af
Geoff Levand <geoff@infradead.org>
2018-01-11 09:59:24 -0800

    scripts: Add libfdt to install deps
    libfdt-dev is needed when building kernels for architectures that
    support a device tree.
    
    Fixes build errors like these when building kvm+lkvm flavor for
    ARM64:
    
      Makefile: No libfdt found. Please install libfdt-dev package. Stop.
    
    Signed-off-by: Geoff Levand <geoff@infradead.org>

--- scripts/install-deps-debian-sid.sh
+++ scripts/install-deps-debian-sid.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-DEBIAN_SID_DEPS="ca-certificates gcc libc6-dev gpg gpg-agent make automake wget git golang-go coreutils cpio squashfs-tools autoconf file xz-utils patch bc locales libacl1-dev libssl-dev libtspi-dev libsystemd-dev python pkg-config zlib1g-dev libglib2.0-dev libpixman-1-dev libcap-dev"
+DEBIAN_SID_DEPS="ca-certificates gcc libc6-dev gpg gpg-agent make automake wget git golang-go coreutils cpio squashfs-tools autoconf file xz-utils patch bc locales libacl1-dev libssl-dev libtspi-dev libsystemd-dev python pkg-config zlib1g-dev libglib2.0-dev libpixman-1-dev libcap-dev libfdt-dev"
 
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
